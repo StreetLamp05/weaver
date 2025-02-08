f91293416dd2835a302e088ef803bb6e6586af05
jasnquyen <jqwertyasmine@gmail.com>
2025-02-08 14:58:18 -0500

    changing font, background, foreground.

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,9 +1,12 @@
-import { Inter, Lusitana } from 'next/font/google';
-
+import { DotGothic16, Inter, Lusitana } from 'next/font/google';
 
 
 export const inter = Inter({subsets: ['latin']});
 export const lusitana = Lusitana({
     weight: ['400' , '700'],
     subsets: ['latin']
+});
+export const dotGothic = DotGothic16({
+    subsets: ['latin'],
+    weight: ['400']
 });
